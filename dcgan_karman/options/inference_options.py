77bf0fa50274c3497f81ca2f5c8395514e3a508b
cdrygala <116795551+cdrygala@users.noreply.github.com>
2025-10-29 14:36:22 +0100

    Update inference_options.py

--- dcgan_karman/options/inference_options.py
+++ dcgan_karman/options/inference_options.py
@@ -18,19 +18,21 @@ class InferenceOptions():
 
     def initialize(self, parser):
         parser = argparse.ArgumentParser()
+        parser.add_argument('--dataset_dir', type=str, default="\home\.datasets", help='the directory to the dataset \path_to\data_folder. Datasets should have the following structure: data_folder->data_subfolder->images')
         parser.add_argument('--seed', type=str, default=False, help='true if a seed should be set. Makes sense for reproducabiloty of experiments')
         parser.add_argument('--gpu_id', type=int, default=0, help='gpu id to run the inference on')
+        parser.add_argument('--folder_dir', type=str, default="dcgan_original", help='the folder to generated images')
         parser.add_argument('--experiment_name', type=str, default='dcgan', help='name of the experiment under which the results will be saved')
-        parser.add_argument('--resize_img', type=int, default=512, help='size k of the images on which the DCGAN has been trained on')
-        parser.add_argument('--channels', type=int, default=1, help='number of channels of the input image')
+        parser.add_argument('--resize_img', type=int, default=256, help='size k of the images on which the DCGAN has been trained on')
+        parser.add_argument('--channels', type=int, default=2, help='number of channels of the input image')
         parser.add_argument('--image_type', type=str, default='karman', help='the type of images the generator has been trained on. Possible choices are: karman, lpt')
         parser.add_argument('--latent_dim', type=int, default=100, help='length of the latent vector which is input for the generator')
         parser.add_argument('--checkpoint_dir', type=str, help='directory of the checkpoints of the generator')
         parser.add_argument('--model_G', type=str, help='name of checkpoint of the generators model')
-        parser.add_argument('--num_images', type=int, default=200, help='number of images to generate')
+        parser.add_argument('--num_images', type=int, default=3000, help='number of images to generate')
+        parser.add_argument('--outer_key', type=int, default=0, help='Choose which dataset to use, when using the ilmenau dataset, possible values: 0-8')
         parser.add_argument('--latent_type', type=str, default='naive', help='approach to compute the latent vectors. Possible choices are: naive, bilinear, bilinear2P, random_walk')
         parser.add_argument('--bi_steps', type=int, default=10, help='the number of steps between each interpolation of two latent vectors')
-        # parser.add_argument('--', type=, default=, help='')
         self.initialized = True
         return parser
 
